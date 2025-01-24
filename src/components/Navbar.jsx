ce68f0f23ea0a9d443ef023809e060095a9bf568
dinhdev-nu <truongbadinh13579@gmail.com>
2025-01-24 14:18:07 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+    
+    return (
+
+        <nav style={ {display: "flex", justifyContent: "center", alignContent: "center", backgroundColor: "aqua"} }>
+            <h1>aaaaaaa</h1>
+        </nav>
+    )
+
+}
+
+export default NavBar
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/Navbar"
+
+
+const HomePage = () => {
+
+    return (
+        <div>
+            < NavBar />
+        </div>
+    )
+
+}
+
+export default HomePage
